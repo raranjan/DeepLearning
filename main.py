00c351531ad63d7152530f47004d502d8e67ba27
Rakesh Ranjan <raks.ranjan@gmail.com>
2019-10-20 16:14:13 +0530

    Created basic structure for the program

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import numpy as np
+
+def compute_error(b, m, points):
+    pass
+
+
+def step_gradient(b_current, m_current, points, learning_rate):
+    pass
+
+
+def gradient_descent_runner(points, initial_b, initial_m, num_iterations, learning_rate):
+    b = initial_b
+    m = initial_m
+
+    for i in range(num_iterations):
+        b, m = step_gradient(b, m, np.array(points), learning_rate)
+
+    return [b, m]
 
 
 def run():
-    pass
+    points = np.genfromtxt('data/data.csv', delimiter=',')
+    
+    #hyperparameters
+    learning_rate=0.0001
+
+    #y = mx + b (slope formula)
+    initial_b = 0
+    initial_m = 0
+
+    num_iterations = 1000
+
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, num_iterations, learning_rate)
 
 
 if __name__ == "__main__":
